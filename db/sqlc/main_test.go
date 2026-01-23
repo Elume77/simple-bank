d88a1c3cb9c611949c4bca140e3fd2b670187256
Elume77 <222136468+Elume77@users.noreply.github.com>
2026-01-23 14:40:26 +0100

    Viper+Env config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,19 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	"tutorial.sqlc.dev/app/utils"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load configuration:", err)
+
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 
