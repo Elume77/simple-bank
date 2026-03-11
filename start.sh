0eb2f5ecd14b7bd954fff2550088b068fc5f6a8b
Elume77 <222136468+Elume77@users.noreply.github.com>
2026-03-11 11:34:21 +0100

    new changes to pull docker image and test connection

--- start.sh
+++ start.sh
@@ -5,10 +5,12 @@ set -e
 
 echo "Running database migrations..."
 # Use the environment variables from your app.env/environment
-source /app/app.env
+# source /app/app.env    
 /usr/local/bin/migrate -path ./db/migration -database "$DB_URL" -verbose up
 
 
 echo "Starting the application..."
 # Exec replaces the shell process with the app process so it receives OS signals (SIGTERM)
-exec ./main
+#exec ./main
+
+exec "$@"
